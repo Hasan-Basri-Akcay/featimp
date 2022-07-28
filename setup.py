efa854a1b1c461398c20f07e28aadd7da8c43504
Hasan Basri Akcay <hasan.basri.akcay@gmail.com>
2022-07-28 17:08:22 +0300

    setup v1

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="featimp",
+    version="0.0.1",
+    author="Hasan Basri Akcay",
+    author_email="hasan.basri.akcay@gmail.com",
+    description="Feature importance for machine learning",
+    long_description=(
+        "Featimp helps with feature understanding, "
+        "calculating feature importances, feature "
+        "debugging, and leakage detection"
+    ),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hasan-Basri-Akcay/featimp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["pandas", "numpy", "matplotlib", "seaborn", 
+                      "scikit-learn", "lightgbm", "catboost", "scipy"],
+    keywords=["python", "data science", "data analysis", "exploratory data analysis", 
+              "feature importance", "beginner"],
+)
