9192eab5e7e65806073ef5ecf43ced2c2129accf
Hasan Basri Akçay <hasan.basri.akcay@gmail.com>
2022-08-01 08:57:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="featimp",
-    version="v0.1.3",
+    version="v0.1.4",
     author="Hasan Basri Akcay",
     author_email="hasan.basri.akcay@gmail.com",
     description="Feature importance for machine learning",
